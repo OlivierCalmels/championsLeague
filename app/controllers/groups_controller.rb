367be1d8e6cb322738a4dda6a014a1cc56958cd5
Olivier Calmels <calmels.o@gmail.com>
2020-12-24 00:18:32 +0100

    group.show added

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,4 +5,10 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def show
+    @tournament = Tournament.find(params[:tournament_id])
+    @group = Group.find(params[:id])
+    @team1 = Team.find(@group.team1_id)
+    @team2 = Team.find(@group.team2_id)
+  end
 end
