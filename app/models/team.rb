c82cc4fed12eac3fc9f3ae55d6ca8a713c9118dd
Olivier Calmels <calmels.o@gmail.com>
2020-12-23 18:18:59 +0100

    group team1 and team 2 ok

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+  has_many :firstTeams, :class_name => "Groups", :foreign_key => 'team1_id'
+  has_many :secondTeams, :class_name => "Groups", :foreign_key => 'team2_id'
 end
