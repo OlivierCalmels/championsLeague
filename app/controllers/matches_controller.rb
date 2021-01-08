ce5580d21672a704fdd679ba85ecf0abfe0a58b2
OlivierCalmels <calmels.o@gmail.com>
2021-01-08 15:04:12 +0100

    groups_display_depends_tournament

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,11 +1,8 @@
 class MatchesController < ApplicationController
-
   def index
     p "----------------------------------"
     p @tournament = Tournament.find(params[:tournament_id])
     p @groups = Group.where(tournament_id: @tournament.id)
     raise
   end
-
-
 end
