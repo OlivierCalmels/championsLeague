250b0f6493998067294ee6799f38f8949068dce9
OlivierCalmels <calmels.o@gmail.com>
2021-07-01 17:19:49 +0200

    add a Mettre la page a jour btn

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'font-awesome-sass'
 
+# gem 'sidekiq'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/jobs/draws_creation_job.rb
+++ app/jobs/draws_creation_job.rb
@@ -1,15 +1,15 @@
 class DrawsCreationJob < ApplicationJob
+  # include Sidekiq
   queue_as :default
 
   def perform(*)
     # Do something later
 
-    p tournament = Tournament.find_by(name:"2020")
-    p dr1 = Draw.create(tournament_id: tournament.id)
-    p name = "Jobs test"
-    p first_team = Team.find_by(name: "Bayern")
-    p second_team = Team.find_by(name: "Monchenglad")
-    p Match.create(draw_id: dr1.id, team1_id: first_team.id, team2_id: second_team.id, name: name )
+    # destroy_draws
+    # draws_maker
     raise
+    Draw.draws_maker
+    raise
+
   end
 end
