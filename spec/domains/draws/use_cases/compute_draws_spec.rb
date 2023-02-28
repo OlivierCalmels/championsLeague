938b13c94cec4f5ca7e578b7d070b8442cb75509
OlivierCalmels <61502515+OlivierCalmels@users.noreply.github.com>
2023-03-01 00:19:40 +0100

    Fix tests

--- spec/domains/draws/use_cases/compute_draws_spec.rb
+++ spec/domains/draws/use_cases/compute_draws_spec.rb
@@ -10,12 +10,12 @@ module Draws
 
       let!(:tournament) { Tournament.create!(name: 'My tournament') }
       let!(:group1) do
-        Group.create!(tournament_id: tournament.id, name: 'My group 1', team1_id: team11.id, team2_id: team12.id)
+        Group.create!(tournament: tournament, name: 'My group 1', team1: team11, team2: team12)
       end
       let!(:team11) { Team.create!(name: 'My team 1 1') }
       let!(:team12) { Team.create!(name: 'My team 1 2') }
       let!(:group2) do
-        Group.create!(tournament_id: tournament.id, name: 'My group 2', team1_id: team21.id, team2_id: team22.id)
+        Group.create!(tournament: tournament, name: 'My group 2', team1: team21, team2: team22)
       end
       let!(:team21) { Team.create!(name: 'My team 2 1') }
       let!(:team22) { Team.create!(name: 'My team 2 2') }
